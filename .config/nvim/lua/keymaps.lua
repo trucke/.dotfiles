b8fbd31e078781a4d64b77dad23cd1975e78eef0
trucke <kt.code@pm.me>
2024-02-24 22:55:35 +0100

    add new keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,14 +1,19 @@
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true }) 
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = 'Open netrw' })
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat the current buffer' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move current line up' })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move current line down' })
 
 -- [[ Highlight on yank ]]
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
 })
