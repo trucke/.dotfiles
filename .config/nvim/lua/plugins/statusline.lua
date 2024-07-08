9e8d3160bec27c32b9844dbadc58740cc637a32a
trucke <kt.code@pm.me>
2024-07-08 13:39:36 +0200

    update neovim config

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -5,6 +5,8 @@ return {
         options = {
             icons_enabled = true,
             theme = 'auto',
+            component_separators = '|',
+            section_separators = { left = '', right = '' },
         },
         sections = {
             lualine_x = { 'filetype' },
