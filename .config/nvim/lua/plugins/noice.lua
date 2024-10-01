261fe04bd6b90758bbafa4847ed03f6b7bdc7fdc
kevthedev <kt.code@pm.me>
2024-10-01 15:33:11 +0200

    feat(nvim): add plugin folke/noice.nvim (experiment)

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("noice").setup({
+                routes = {
+                    {
+                        view = "notify",
+                        filter = { event = "msg_showmode" },
+                    },
+                },
+                views = {
+                    cmdline_popup = {
+                        position = {
+                            row = "50%",
+                            col = "50%",
+                        },
+                        size = {
+                            width = 60,
+                            height = "auto",
+                        },
+                    },
+                    popupmenu = {
+                        relative = "editor",
+                        position = {
+                            row = "65%",
+                            col = "50%",
+                        },
+                        size = {
+                            width = 60,
+                            height = 10,
+                        },
+                        border = {
+                            style = "rounded",
+                            padding = { 0, 1 },
+                        },
+                        win_options = {
+                            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                        },
+                    },
+                },
+            })
+        end,
+    }
+}
