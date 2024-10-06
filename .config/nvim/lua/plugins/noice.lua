80f964563716501686f705c3bb088fc2a26b2699
kevthedev <kt.code@pm.me>
2024-10-06 21:25:34 +0200

    chore(nvim): update noice config

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,51 +1,29 @@
 return {
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {},
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            },
         },
-        config = function()
-            require("noice").setup({
-                routes = {
-                    {
-                        view = "notify",
-                        filter = { event = "msg_showmode" },
-                    },
-                },
-                views = {
-                    cmdline_popup = {
-                        position = {
-                            row = "50%",
-                            col = "50%",
-                        },
-                        size = {
-                            width = 60,
-                            height = "auto",
-                        },
-                    },
-                    popupmenu = {
-                        relative = "editor",
-                        position = {
-                            row = "65%",
-                            col = "50%",
-                        },
-                        size = {
-                            width = 60,
-                            height = 10,
-                        },
-                        border = {
-                            style = "rounded",
-                            padding = { 0, 1 },
-                        },
-                        win_options = {
-                            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-                        },
-                    },
-                },
-            })
-        end,
-    }
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+        },
+    },
+    keys = {
+        { "<leader>nd", function() require('noice').cmd('dismiss') end,   desc = "Noice Dismiss" },
+        { "<leader>nh", function() require('noice').cmd('telescope') end, desc = "Noice History" },
+        { "<leader>ne", function() require('noice').cmd('errors') end,    desc = "Noice Errors" },
+    },
 }
---
+++ .config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,9 @@
+return {
+    "rcarriga/nvim-notify",
+    opts = {
+        render = "compact",
+        stages = "slide",
+        timeout = 1000,
+        top_down = false,
+    },
+}
